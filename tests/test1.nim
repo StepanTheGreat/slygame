a2cc57a7547edc43eaf86cf201f7fcd72b88ec14
StepanTheGreat <grettanumber900@gmail.com>
2023-07-31 13:32:29 +0200

    Corrected test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,29 +1,16 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
-#import unittest
+import unittest
 import slygame, slygame/display, slygame/time
 
-const FPS = 20
+# The test should just open a window, and close it with no errors.
 
-var clock = time.newClock()
-var screen = display.set_mode((800, 800), "Super window")
-
-var close_on = 0
-while true:
-    clock.tick(FPS)
-
-    echo close_on
-    close_on += 1
-    if close_on > 180:
-        break
+test "Window init":
+    var screen = display.set_mode((800, 800), "Test window")
 
     screen.fill((0, 0, 255, 255))
     screen.flip()
 
-time.delay(1000)
-slygame.quit()
+    time.delay(1000)
+
+    slygame.quit()
+    check true
+    
