280ba71ebe07e8716bd08f6bde20fb6e9f904acf
StepanTheGreat <grettanumber900@gmail.com>
2023-07-31 20:11:04 +0200

    Events

--- src/slygame.nim
+++ src/slygame.nim
@@ -1,5 +1,15 @@
 import sdl2
 
+const 
+    QUIT* = sdl2.QuitEvent.uint
+    ACTIVEEVENT* = sdl2.WindowEvent_FocusGained.uint
+    KEYDOWN* = sdl2.KeyDown.uint
+    KEYUP* = sdl2.KeyDown.uint
+    MOUSEMOTION* = sdl2.MouseMotion.uint
+    MOUSEBUTTONUP* = sdl2.MouseButtonUp.uint
+    MOUSEBUTTONDOWN* = sdl2.MouseButtonDown.uint
+    USEREVENT* = sdl2.UserEvent.uint
+
 proc init*() =
     sdl2.init(sdl2.INIT_EVERYTHING)
 
